2a816556beb6e6550b361d49ed79ecbb13198822
HP-Z240SFF\xieao <xieaofan2006@outlook.com>
2022-03-30 18:00:25 +0800

    0.1.1

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-# 这是一个示例 Python 脚本。
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtCore
+import random
+import time
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+import ui
+
+class start():
+    def __init__(self, ui):
+        self.app = QApplication(sys.argv)
+        self.ui = ui.Ui_mainWindow()
+        self.start_n = 1
+        self.end_n = 54
+        self.time = QtCore.QTimer()
+        self.time.timeout.connect(self.show)
+        self.tn = 0
+        self.pn = 0
+        self.name = 'Name'
+
+    def setup(self):
+        MainWindow = QMainWindow()
+        ui = self.ui
+        ui.setupUi(MainWindow)
+        MainWindow.show()
+        ui.pushButton.clicked.connect(self.start_ramdom)
+        sys.exit(self.app.exec_())
+
+    def show(self):
+        n = self.tn % self.end_n
+        self.ui.lcdNumber.setProperty("intValue", n)
+        if self.tn == self.pn:
+            self.time.stop()
+            _translate = QtCore.QCoreApplication.translate
+            self.ui.label.setText(_translate("mainWindow", self.name))
+            self.ui.pushButton.setEnabled(True)
+        self.tn = self.tn + 1
+
+    def start_ramdom(self):
+        ui = self.ui
+        tn = random.randint(self.start_n, self.end_n)
+        self.tn = 0
+        self.pn = tn+self.end_n*random.randint(1,2)
+        '''
+        while t < tn:
+            #time.sleep(0.1)
+            t = t + 1
+            ui.lcdNumber.setProperty("intValue", t)
+        '''
+        self.ui.pushButton.setEnabled(False)
+        self.time.start(20)
 
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+if __name__ == "__main__":
+    n = start(ui)
+    n.setup()
 
 
-# 按间距中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
